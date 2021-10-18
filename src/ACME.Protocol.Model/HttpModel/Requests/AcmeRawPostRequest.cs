21daa550a66540ad4818af9f40621064ba9b8ef4
Thomas Glatzer <glatzert@outlook.de>
2021-10-18 21:04:43 +0200

    Fix a serialization error

--- src/ACME.Protocol.Model/HttpModel/Requests/AcmeRawPostRequest.cs
+++ src/ACME.Protocol.Model/HttpModel/Requests/AcmeRawPostRequest.cs
@@ -8,8 +8,6 @@ namespace TGIT.ACME.Protocol.HttpModel.Requests
         private string? _header;
         private string? _signature;
 
-        private AcmeRawPostRequest() { }
-
         [JsonPropertyName("protected")]
         public string Header { 
             get => _header ?? throw new NotInitializedException();
