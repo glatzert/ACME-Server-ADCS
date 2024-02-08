16d4c50508b8ca65a26c5ab961ecaf015c358df1
Ottenhus, Thomas <ottenhus@uni-mainz.de>
2024-02-08 12:13:01 +0100

    Remove usings

--- src/ACME.Protocol.Model/Model/Identifier.cs
+++ src/ACME.Protocol.Model/Model/Identifier.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.Serialization;
 using TGIT.ACME.Protocol.Model.Exceptions;
