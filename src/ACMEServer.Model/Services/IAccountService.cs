9797e2fcb16011409660e3d634536824c58d1346
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-08-04 14:59:37 +0200

    Readd methods to interfaces

--- src/ACMEServer.Model/Services/IAccountService.cs
+++ src/ACMEServer.Model/Services/IAccountService.cs
@@ -4,13 +4,13 @@ namespace Th11s.ACMEServer.Model.Services
     public interface IAccountService
     {
         Task<Account> CreateAccountAsync(Jwk jwk, List<string>? contact, bool termsOfServiceAgreed, CancellationToken cancellationToken);
-        Task UpdateAccountAsync(Account account, List<string>? contact, AccountStatus? status, CancellationToken requestAborted);
+        Task<Account> UpdateAccountAsync(Account account, List<string>? contact, AccountStatus? status, CancellationToken cancellationToken);
 
         Task<Account?> FindAccountAsync(Jwk jwk, CancellationToken cancellationToken);
 
         Task<Account> FromRequestAsync(CancellationToken cancellationToken);
         Task<Account?> LoadAcountAsync(string accountId, CancellationToken cancellationToken);
 
-        Task<IEnumerable<string>> GetOrderIdsAsync(Account account, CancellationToken requestAborted);
+        Task<List<string>> GetOrderIdsAsync(Account account, CancellationToken requestAborted);
     }
 }
