dc85edd3382a3a532d09aac298c099fef2446db2
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-08-04 14:51:49 +0200

    Readd update account and GetOrderIds

--- src/ACMEServer.Model/Services/IAccountService.cs
+++ src/ACMEServer.Model/Services/IAccountService.cs
@@ -1,14 +1,16 @@
-﻿namespace Th11s.ACMEServer.Model.Services
+﻿
+namespace Th11s.ACMEServer.Model.Services
 {
     public interface IAccountService
     {
-        Task<Account> CreateAccountAsync(Jwk jwk, List<string>? contact,
-            bool termsOfServiceAgreed, CancellationToken cancellationToken);
+        Task<Account> CreateAccountAsync(Jwk jwk, List<string>? contact, bool termsOfServiceAgreed, CancellationToken cancellationToken);
+        Task UpdateAccountAsync(Account account, List<string>? contact, AccountStatus? status, CancellationToken requestAborted);
 
         Task<Account?> FindAccountAsync(Jwk jwk, CancellationToken cancellationToken);
 
+        Task<Account> FromRequestAsync(CancellationToken cancellationToken);
         Task<Account?> LoadAcountAsync(string accountId, CancellationToken cancellationToken);
 
-        Task<Account> FromRequestAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<string>> GetOrderIdsAsync(Account account, CancellationToken requestAborted);
     }
 }
