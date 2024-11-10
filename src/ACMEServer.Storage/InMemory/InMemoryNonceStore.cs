2fd3a59e800cdd7cdda8ba84be7776be43e2c930
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-11-10 21:57:13 +0100

    Simple integration tests

---
+++ src/ACMEServer.Storage/InMemory/InMemoryNonceStore.cs
@@ -0,0 +1,18 @@
+﻿using Th11s.ACMEServer.Model;
+using Th11s.ACMEServer.Model.Storage;
+
+namespace ACMEServer.Storage.InMemory;
+
+public class InMemoryNonceStore : INonceStore
+{
+    private readonly HashSet<string> _nonces = [];
+
+    public Task SaveNonceAsync(Nonce nonce, CancellationToken cancellationToken)
+    {
+        _nonces.Add(nonce.Token);
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> TryRemoveNonceAsync(Nonce nonce, CancellationToken cancellationToken) 
+        => Task.FromResult(_nonces.Remove(nonce.Token));
+}
