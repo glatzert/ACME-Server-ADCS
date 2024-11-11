a26a3ca17769a2804ec2e6de561a8f1a1a339a2d
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-11-11 22:36:54 +0100

    Order Process Test

---
+++ tests/ACMEServer.ADCS.IntegrationTests/FakeChallengeValidator.cs
@@ -0,0 +1,19 @@
+﻿using Th11s.ACMEServer.Model;
+using Th11s.ACMEServer.Model.Services;
+
+namespace ACMEServer.ADCS.IntegrationTests;
+
+internal class FakeChallengeValidator : IChallengeValidator
+{
+    public FakeChallengeValidator(string challengeType)
+    {
+        ChallengeType = challengeType;
+    }
+
+    public string ChallengeType { get; }
+
+    public Task<ChallengeValidationResult> ValidateChallengeAsync(Challenge challenge, Account account, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new ChallengeValidationResult(ChallengeResult.Valid, null));
+    }
+}
