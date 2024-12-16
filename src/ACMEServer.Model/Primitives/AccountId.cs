f7002db9a160f2cd09e8d94c036550fd50a19676
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-12-16 22:00:09 +0100

    Add Id primitives for later use

---
+++ src/ACMEServer.Model/Primitives/AccountId.cs
@@ -0,0 +1,27 @@
+﻿namespace Th11s.ACMEServer.Model.Primitives;
+
+public readonly struct AccountId
+{
+    public AccountId()
+    {
+        Value = GuidString.NewValue();
+    }
+
+    public AccountId(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static implicit operator string(AccountId accountId) => accountId.Value;
+    public static explicit operator AccountId(string value) => new(value);
+
+    public override readonly string ToString() => Value;
+    
+    public override readonly bool Equals(object? obj) => obj is AccountId other && Value == other.Value;
+    public override readonly int GetHashCode() => HashCode.Combine(Value);
+
+    public static bool operator ==(AccountId left, AccountId right) => left.Equals(right);
+    public static bool operator !=(AccountId left, AccountId right) => !left.Equals(right);
+}
