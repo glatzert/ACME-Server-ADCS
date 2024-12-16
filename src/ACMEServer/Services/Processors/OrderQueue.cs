0d4e8675f4fdbc10f60cd556e13b7c4f215dce5b
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-12-16 22:59:04 +0100

    Channel based queue for pub/sub

---
+++ src/ACMEServer/Services/Processors/OrderQueue.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+using Th11s.ACMEServer.Model.Primitives;
+
+namespace Th11s.ACMEServer.Services.Processors
+{
+
+    public sealed class OrderQueue : IDisposable
+    {
+        private readonly Channel<OrderId> _channel;
+
+        public OrderQueue()
+        {
+            _channel = Channel.CreateUnbounded<OrderId>();
+        }
+
+        public void Enqueue(OrderId item)
+            => _channel.Writer.TryWrite(item);
+
+        public IAsyncEnumerable<OrderId> DequeueAllAsync(CancellationToken cancellationToken)
+            => _channel.Reader.ReadAllAsync(cancellationToken);
+
+        public void Dispose()
+        {
+            _channel.Writer.Complete();
+        }
+    }
+}
