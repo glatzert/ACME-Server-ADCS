fc1c0abe481bdab4d0cb8a99d656b2068362afec
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-03-07 12:11:46 +0100

    Add error for EAB failure

--- src/ACMEServer.Model/Exceptions/ExternalAccountBindingException.cs
+++ src/ACMEServer.Model/Exceptions/ExternalAccountBindingException.cs
@@ -8,4 +8,11 @@
 
         public override string ErrorType => "externalAccountRequired";
     }
+
+    public class  ExternalAccountBindingFailedException : CustomAcmeException
+    {
+        public ExternalAccountBindingFailedException(string detailMessage) : base($"External account binding error: {detailMessage}") { }
+
+        public override string ErrorType => "externalAccountInvalid";
+    }
 }
