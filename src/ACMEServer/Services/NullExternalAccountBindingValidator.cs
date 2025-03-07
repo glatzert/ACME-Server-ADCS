8cb924329c25892b3caba7702e591fab8aa14f79
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-03-07 14:50:35 +0100

    Add NullEAB Exception

--- src/ACMEServer/Services/NullExternalAccountBindingValidator.cs
+++ src/ACMEServer/Services/NullExternalAccountBindingValidator.cs
@@ -1,11 +1,12 @@
-﻿using Th11s.ACMEServer.Model.JWS;
+﻿using Th11s.ACMEServer.Model.Exceptions;
+using Th11s.ACMEServer.Model.JWS;
 
 namespace Th11s.ACMEServer.Services;
 
 public class NullExternalAccountBindingValidator : IExternalAccountBindingValidator
 {
-    public Task<bool> ValidateExternalAccountBindingAsync(AcmeJwsHeader requestHeader, AcmeJwsToken? externalAccountBinding, CancellationToken ct)
+    public Task ValidateExternalAccountBindingAsync(AcmeJwsHeader requestHeader, AcmeJwsToken? externalAccountBinding, CancellationToken ct)
     {
-        return Task.FromResult(false);
+        throw new ExternalAccountBindingFailedException("External account binding is not supported.");
     }
 }
