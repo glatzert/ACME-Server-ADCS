2ae75343067c5c81edf1d606e5f52dc518450c43
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-03-08 14:36:59 +0100

    add acme:error prefix

--- src/ACMEServer.Model/AcmeErrors.cs
+++ src/ACMEServer.Model/AcmeErrors.cs
@@ -2,150 +2,152 @@
 
 public static class AcmeErrors
 {
+    public const string AcmeUrn = "urn:ietf:params:acme:error";
+
     public static AcmeError AccountDoesNotExist()
         => new(
-            "accountDoesNotExist", 
+            $"{AcmeUrn}:accountDoesNotExist", 
             "The request specified an account that does not exist"
         );
 
     public static AcmeError AlreadyRevoked()
         => new(
-            "alreadyRevoked", 
+            $"{AcmeUrn}:alreadyRevoked", 
             "Certificate has already been revoked."
         );
 
     public static AcmeError BadCSR(string detail)
         => new(
-            "badCSR", 
+            $"{AcmeUrn}:badCSR", 
             $"The CSR is unacceptable: {detail}"
         );
 
     public static AcmeError BadNonce()
         => new(
-            "badNonce", 
+            $"{AcmeUrn}:badNonce", 
             "The client sent an unaccaptable anti-replay nonce."
         );
 
     public static AcmeError BadPublicKey(string detail)
         => new(
-            "badPublicKey", 
+            $"{AcmeUrn}:badPublicKey", 
             $"The JWS was signed by a public key the server does not support: {detail}");
 
     public static AcmeError BadRevocationReason(string detail)
         => new(
-            "badRevocationReason", 
+            $"{AcmeUrn}:badRevocationReason", 
             $"The revocation reason provided is not allowed by the server: {detail}"
         );
 
     public static AcmeError BadSignatureAlgorithm(string detail)
         => new(
-            "badSignatureAlgorithm",
+            $"{AcmeUrn}:badSignatureAlgorithm",
             $"The JWS was signed with an algorithm the server does not support: {detail}"
         );
 
     public static AcmeError CAA()
         => new(
-            "caa",
+            $"{AcmeUrn}:caa",
             "Certification Authority Authorization (CAA) records forbid the CA from issuing a certificate."
         );
 
 
     public static AcmeError Compound(string detail, IEnumerable<AcmeError> subErrors)
         => new(
-            "compound", 
+            $"{AcmeUrn}:compound", 
             "Multiple errors occured.", 
             subErrors: subErrors);
 
 
     public static AcmeError Connection(Identifier identifier)
         => new(
-            "connection",
+            $"{AcmeUrn}:connection",
             "The server could not connect to the validation target.",
             identifier: identifier
             );
 
     public static AcmeError Dns(Identifier identifier)
         => new(
-            "dns",
+            $"{AcmeUrn}:dns",
             "There was a problem with a DNS query during identifier validation.",
             identifier: identifier
             );
 
     public static AcmeError ExternalAccountRequired()
         => new(
-            "externalAccountRequired",
+            $"{AcmeUrn}:externalAccountRequired",
             "The request must include a value for the \"externalAccountBinding\" field."
             );
 
     public static AcmeError IncorrectResponse(Identifier identifier)
         => new(
-            "incorrectResponse",
+            $"{AcmeUrn}:incorrectResponse",
             "Response received didn't match the challenge's requirements",
             identifier: identifier
             );
 
     public static AcmeError InvalidContact(string contact)
         => new(
-            "invalidContact",
+            $"{AcmeUrn}:invalidContact",
             $"A contact URL for an account was invalid: {contact}"
             );
 
     public static AcmeError MalformedRequest(string detail)
         => new(
-            "malformedRequest", 
+            $"{AcmeUrn}:malformedRequest", 
             $"The request message was malformed: {detail}"
             );
 
     public static AcmeError OrderNotReady()
         => new(
-            "orderNotReady",
+            $"{AcmeUrn}:orderNotReady",
             "The request attempted to finalize an order that is not ready."
             );
 
     public static AcmeError RateLimited()
         => new(
-            "rateLimited",
+            $"{AcmeUrn}:rateLimited",
             "The request exceeds a rate limit."
             );
 
     public static AcmeError RejectedIdentifier(Identifier identifier)
         => new(
-            "rejectedIdentifier",
+            $"{AcmeUrn}:rejectedIdentifier",
             "The server will not issue certificates for the identifier.", 
             identifier);
 
     public static AcmeError ServerInternal()
         => new(
-            "serverInternal",
+            $"{AcmeUrn}:serverInternal",
             "The server experienced an internal error."
             );
 
     public static AcmeError Tls(Identifier identifier)
         => new(
-            "tls",
+            $"{AcmeUrn}:tls",
             "The server received a TLS error during validation.",
             identifier: identifier
             );
 
     public static AcmeError Unauthorized()
         => new(
-            "unauthorized",
+            $"{AcmeUrn}:unauthorized",
             "The client lacks sufficient authorization."
             );
     public static AcmeError UnsupportedContact(string contact)
         => new(
-            "unsupportedContact",
+            $"{AcmeUrn}:unsupportedContact",
             $"A contact URL for an account used an unsupported protocol scheme: {contact}"
             );
     public static AcmeError UnsupportedIdentifier(string detail, Identifier identifier)
         => new(
-            "unsupportedIdentifier",
+            $"{AcmeUrn}:unsupportedIdentifier",
             "An identifier is of an unsupported type.", 
             identifier
             );
     public static AcmeError UserActionRequired(string detail)
         => new(
-            "userActionRequired", 
+            $"{AcmeUrn}:userActionRequired", 
             detail
             );
 }
