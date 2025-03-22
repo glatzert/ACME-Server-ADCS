0f59e3d297fbed4c8a0d8b3328ba182fe82227e7
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-03-22 22:44:47 +0100

    Minimal API model Binding

--- src/ACMEServer.HttpModel/Requests/AcmePayload.cs
+++ src/ACMEServer.HttpModel/Requests/AcmePayload.cs
@@ -1,4 +1,9 @@
-﻿namespace Th11s.ACMEServer.HttpModel.Requests
+﻿using Microsoft.AspNetCore.Http;
+using System.Reflection;
+using System.Text.Json;
+using Th11s.ACMEServer.Model.Features;
+
+namespace Th11s.ACMEServer.HttpModel.Requests
 {
     public class AcmePayload<TPayload>
     {
@@ -8,5 +13,12 @@
         }
 
         public TPayload Value { get; }
+
+
+        public static ValueTask<AcmePayload<TPayload>> BindAsync(HttpContext httpContext, ParameterInfo parameterInfo)
+        {
+            var payload = httpContext.Features.Get<AcmeRequestFeature>()?.Request.AcmePayload ?? throw new InvalidOperationException();
+            return ValueTask.FromResult(new AcmePayload<TPayload>(payload.Deserialize<TPayload>()));
+        }
     }
 }
