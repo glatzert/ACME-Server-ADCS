91a4538bcb553b1afb1228678a040be964038222
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-03-22 12:37:05 +0100

    Better exception tests

--- tests/ACMEServer.Tests/Integration/AccountManagementTests.cs
+++ tests/ACMEServer.Tests/Integration/AccountManagementTests.cs
@@ -60,14 +60,8 @@ public class AccountManagementTests
     {
         var acme = await CreateAcmeContextAsync();
 
-        try
-        {
-            var account =  await acme.Account();
-        }
-        catch (AcmeRequestException ex)
-        {
-            Assert.Contains("urn:ietf:params:acme:error:accountDoesNotExist", ex.Message);
-        }
+        var ex = await Assert.ThrowsAsync<AcmeRequestException>(() => acme.Account());
+        Assert.Contains("urn:ietf:params:acme:error:accountDoesNotExist", ex.Message);
     }
 
     [Fact]
@@ -75,16 +69,7 @@ public class AccountManagementTests
     {
         var acme = await CreateAcmeContextAsync();
 
-        AcmeRequestException? newAccountException = null;
-        try {
-            var account = await acme.NewAccount("test@example.com", false);
-        }
-        catch (AcmeRequestException ex)
-        {
-            newAccountException = ex;
-        }
-
-        Assert.NotNull(newAccountException);
+        var newAccountException = await Assert.ThrowsAsync<AcmeRequestException>(()=> acme.NewAccount("test@example.com", false));
         Assert.Contains("urn:ietf:params:acme:error:userActionRequired", newAccountException?.Message);
     }
 }
