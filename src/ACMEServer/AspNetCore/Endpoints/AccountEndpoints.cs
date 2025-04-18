33edc9c6b5b4932575a5bb3dd0505ddc23fcd4bf
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-04-18 13:46:50 +0200

    Rewrite CreateOrGetAccount

--- src/ACMEServer/AspNetCore/Endpoints/AccountEndpoints.cs
+++ src/ACMEServer/AspNetCore/Endpoints/AccountEndpoints.cs
@@ -6,6 +6,7 @@ using Th11s.ACMEServer.AspNetCore.Authorization;
 using Th11s.ACMEServer.AspNetCore.Extensions;
 using Th11s.ACMEServer.HttpModel;
 using Th11s.ACMEServer.HttpModel.Requests;
+using Th11s.ACMEServer.Model;
 using Th11s.ACMEServer.Model.Exceptions;
 using Th11s.ACMEServer.Model.JWS;
 using Th11s.ACMEServer.Model.Services;
@@ -32,31 +33,32 @@ namespace Th11s.ACMEServer.AspNetCore.Endpoints
         }
 
 
-        public static Task<IResult> CreateOrGetAccount(
-            AcmeJwsHeader header, 
-            AcmePayload<CreateOrGetAccount> payload, 
-            IAccountService accountService,
+        public static async Task<IResult> CreateOrGetAccount(
             HttpContext httpContext,
+            AcmeJwsHeader header, 
+            IAccountService accountService,
             LinkGenerator linkGenerator,
             CancellationToken ct)
         {
-            if (payload.Value.OnlyReturnExisting)
-                return FindAccountAsync(header, accountService, httpContext, linkGenerator, ct);
+            var acmeRequest = httpContext.GetAcmeRequest();
+            if(!acmeRequest.TryGetPayload<CreateOrGetAccount>(out var payload) || payload is null)
+                throw AcmeErrors.MalformedRequest("Payload was empty or could not be read.").AsException();
 
-            return CreateAccountAsync(header, payload, accountService, httpContext, linkGenerator, ct);
-        }
-
-        private static async Task<IResult> CreateAccountAsync(AcmeJwsHeader header, AcmePayload<CreateOrGetAccount> payload, IAccountService accountService, HttpContext httpContext, LinkGenerator linkGenerator, CancellationToken ct)
-        {
-            if (payload == null)
-                throw new MalformedRequestException("Payload was empty or could not be read.");
-
-            var account = await accountService.CreateAccountAsync(
-                header, //Post requests are validated, JWK exists.
-                payload.Value.Contact,
-                payload.Value.TermsOfServiceAgreed,
-                payload.Value.ExternalAccountBinding,
-                ct);
+            Model.Account? account = null;
+            if (payload.OnlyReturnExisting)
+            {
+                account = await accountService.FindAccountAsync(header.Jwk!, ct)
+                    ?? throw AcmeErrors.AccountDoesNotExist().AsException();
+            }
+            else
+            {
+                account = await accountService.CreateAccountAsync(
+                    header, //Post requests are validated, JWK exists.
+                    payload.Contact,
+                    payload.TermsOfServiceAgreed,
+                    payload.ExternalAccountBinding,
+                    ct);
+            }
 
             var accountResponse = new HttpModel.Account(account)
             {
@@ -66,22 +68,11 @@ namespace Th11s.ACMEServer.AspNetCore.Endpoints
             httpContext.AddLocationResponseHeader(linkGenerator, EndpointNames.GetAccount, new { accountId = account.AccountId });
             var accountUrl = linkGenerator.GetUriByName(httpContext, EndpointNames.GetAccount, new { accountId = account.AccountId });
 
-            return Results.Created(accountUrl, accountResponse);
+            return payload.OnlyReturnExisting
+                ? Results.Ok(accountResponse)
+                : Results.Created(accountUrl, accountResponse);
         }
 
-        private static async Task<IResult> FindAccountAsync(AcmeJwsHeader header, IAccountService accountService, HttpContext httpContext, LinkGenerator linkGenerator, CancellationToken ct)
-        {
-            var account = await accountService.FindAccountAsync(header.Jwk!, ct)
-                ?? throw Model.AcmeErrors.AccountDoesNotExist().AsException();
-
-            var accountResponse = new HttpModel.Account(account)
-            {
-                Orders = linkGenerator.GetUriByName(httpContext, EndpointNames.GetOrderList, new { accountId = account.AccountId })
-            };
-
-            httpContext.AddLocationResponseHeader(linkGenerator, EndpointNames.GetAccount, new { accountId = account.AccountId });
-            return Results.Ok(accountResponse);
-        }
 
         public static async Task<IResult> SetAccount(string accountId, AcmePayload<UpdateAccount> payload, IAccountService accountService, HttpContext httpContext, LinkGenerator linkGenerator, CancellationToken ct)
         {
