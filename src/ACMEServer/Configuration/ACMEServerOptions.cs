603c3cffae06bf7edce7a23134bf75c8d70c2ea5
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-04-21 13:13:02 +0200

    Add a todo

--- src/ACMEServer/Configuration/ACMEServerOptions.cs
+++ src/ACMEServer/Configuration/ACMEServerOptions.cs
@@ -2,6 +2,7 @@
 
 public class ACMEServerOptions
 {
+    // TODO: Background checking the status of orders and authorizations is probably not neccessary anymore - we should only do it upon startup to populate the queues.
     public BackgroundServiceOptions HostedWorkers { get; set; } = new ();
 
     public string? WebsiteUrl { get; set; }
