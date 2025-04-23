036de6929318f1c8d45515c9ee5150b222c84f8d
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-04-23 21:38:39 +0200

    Device Attest 01 stubs

---
+++ src/ACMEServer/Services/ChallengeValidation/DeviceAttest01Validator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
+using Th11s.ACMEServer.Model;
+using Th11s.ACMEServer.Model.Extensions;
+
+namespace Th11s.ACMEServer.Services.ChallengeValidation;
+
+public sealed class DeviceAttest01ChallengeValidator(ILogger<DeviceAttest01ChallengeValidator> logger) : ChallengeValidator(logger)
+{
+    private class ChallengePayload
+    {
+        [JsonPropertyName("attObj")]
+        public required string AttestationObject { get; set; }
+    }
+
+    public override string ChallengeType => ChallengeTypes.DeviceAttest01;
+    public override IEnumerable<string> SupportedIdentiferTypes => [IdentifierTypes.PermanentIdentifier];
+
+    protected override async Task<ChallengeValidationResult> ValidateChallengeInternalAsync(Challenge challenge, Account account, CancellationToken cancellationToken)
+    {
+        if(challenge.Payload is null)
+        {
+            return new(ChallengeResult.Invalid, AcmeErrors.IncorrectResponse(challenge.Authorization.Identifier, "The challenge payload was empty."));
+        }
+
+        // Deserialize the outer challenge payload
+        var challengePayload = challenge.Payload.DeserializeBase64UrlEncodedJson<ChallengePayload>();
+        if(challengePayload?.AttestationObject is null)
+        {
+            return new(ChallengeResult.Invalid, AcmeErrors.IncorrectResponse(challenge.Authorization.Identifier, "The attestation object was empty."));
+        }
+
+        throw new NotImplementedException();
+    }
+}
