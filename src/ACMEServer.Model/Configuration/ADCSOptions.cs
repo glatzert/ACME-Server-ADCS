8c8687336d25d104f11dd9d026dfb0a15d2d8d51
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-05-11 20:56:52 +0200

    Make props required

--- src/ACMEServer.Model/Configuration/ADCSOptions.cs
+++ src/ACMEServer.Model/Configuration/ADCSOptions.cs
@@ -3,5 +3,5 @@ namespace Th11s.ACMEServer.Model.Configuration;
 public class ADCSOptions
 {
     public required string CAServer { get; set; }
-    public string? TemplateName { get; set; }
+    public required string? TemplateName { get; set; }
 }
