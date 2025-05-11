7ac219bdf08ab3c67b380634a27f3d0dfb5cdd70
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-05-11 21:26:10 +0200

    Disable the manual testing program for now

--- tests/ACME.CertProvider.ADCS.Tests.Manual/Program.cs
+++ tests/ACME.CertProvider.ADCS.Tests.Manual/Program.cs
@@ -1,97 +1,97 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using Th11s.ACMEServer.CertProvider.ADCS;
-using Th11s.ACMEServer.Model;
-using Th11s.ACMEServer.Model.Configuration;
+﻿//using Microsoft.Extensions.Logging.Abstractions;
+//using System.Security.Cryptography;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using Th11s.ACMEServer.CertProvider.ADCS;
+//using Th11s.ACMEServer.Model;
+//using Th11s.ACMEServer.Model.Configuration;
 
-if (args.Length <= 1)
-{
-    Console.WriteLine("Useage: ACME.CertProvider.ACDS.Tests.Manual.exe CSRValidation");
-    Console.WriteLine("Useage: ACME.CertProvider.ACDS.Tests.Manual.exe Issuance \"<CAConfig>\" \"<CertTemplate>\" \"<DNSName>\"");
-}
+//if (args.Length <= 1)
+//{
+//    Console.WriteLine("Useage: ACME.CertProvider.ACDS.Tests.Manual.exe CSRValidation");
+//    Console.WriteLine("Useage: ACME.CertProvider.ACDS.Tests.Manual.exe Issuance \"<CAConfig>\" \"<CertTemplate>\" \"<DNSName>\"");
+//}
 
-if (args[0] == "CSRValidation")
-{
-    await ManualCSRValidationTest();
-}
-else if (args[0] == "Issuance")
-{
-    await ManualIssuanceTest(args);
-}
+//if (args[0] == "CSRValidation")
+//{
+//    await ManualCSRValidationTest();
+//}
+//else if (args[0] == "Issuance")
+//{
+//    await ManualIssuanceTest(args);
+//}
 
 Console.ReadLine();
 
-async Task ManualCSRValidationTest()
-{
-    var base64Csr = "";
+//async Task ManualCSRValidationTest()
+//{
+//    var base64Csr = "";
 
-    var adcsOptions = new Microsoft.Extensions.Options.OptionsWrapper<ADCSOptions>(
-        new ADCSOptions
-        {
-            CAServer = "",
-            TemplateName = ""
-        });
+//    var adcsOptions = new Microsoft.Extensions.Options.OptionsWrapper<ADCSOptions>(
+//        new ADCSOptions
+//        {
+//            CAServer = "",
+//            TemplateName = ""
+//        });
 
-    var csrValidator = new CSRValidator(adcsOptions, new NullLogger<CSRValidator>());
+//    var csrValidator = new CSRValidator(adcsOptions, new NullLogger<CSRValidator>());
 
-    var validationResult = await csrValidator.ValidateCsrAsync(
-        new Order("FakeAccountId", [new Identifier("dns", "www.test.uni-mainz.de")]),
-        base64Csr,
-        default
-    );
-}
+//    var validationResult = await csrValidator.ValidateCsrAsync(
+//        new Order("FakeAccountId", [new Identifier("dns", "www.test.uni-mainz.de")]),
+//        base64Csr,
+//        default
+//    );
+//}
 
-async Task ManualIssuanceTest(string[] args)
-{
-    if (args.Length != 3)
-    {
-        Console.WriteLine("Useage: ACME.CertProvider.ACDS.Tests.Manual.exe Issuance \"<CAConfig>\" \"<CertTemplate>\" \"<DNSName>\"");
-        return;
-    }
+//async Task ManualIssuanceTest(string[] args)
+//{
+//    if (args.Length != 3)
+//    {
+//        Console.WriteLine("Useage: ACME.CertProvider.ACDS.Tests.Manual.exe Issuance \"<CAConfig>\" \"<CertTemplate>\" \"<DNSName>\"");
+//        return;
+//    }
 
-    var caConfig = args[0];
-    var caTemplate = args[1];
-    var dnsName = args[2];
+//    var caConfig = args[0];
+//    var caTemplate = args[1];
+//    var dnsName = args[2];
 
-    var subjectName = $"CN={dnsName}";
+//    var subjectName = $"CN={dnsName}";
 
-    var csp = RSA.Create(2048);
-    var csr = new CertificateRequest(subjectName, csp, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+//    var csp = RSA.Create(2048);
+//    var csr = new CertificateRequest(subjectName, csp, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
-    var sanBuilder = new SubjectAlternativeNameBuilder();
-    sanBuilder.AddDnsName(dnsName);
-    csr.CertificateExtensions.Add(sanBuilder.Build());
+//    var sanBuilder = new SubjectAlternativeNameBuilder();
+//    sanBuilder.AddDnsName(dnsName);
+//    csr.CertificateExtensions.Add(sanBuilder.Build());
 
-    var csrBytes = csr.CreateSigningRequest();
-    var csrPEM = $"{Convert.ToBase64String(csrBytes)}";
+//    var csrBytes = csr.CreateSigningRequest();
+//    var csrPEM = $"{Convert.ToBase64String(csrBytes)}";
 
-    var acdsOptions = new Microsoft.Extensions.Options.OptionsWrapper<ADCSOptions>(
-        new ADCSOptions
-        {
-            CAServer = caConfig,
-            TemplateName = caTemplate
-        });
+//    var acdsOptions = new Microsoft.Extensions.Options.OptionsWrapper<ADCSOptions>(
+//        new ADCSOptions
+//        {
+//            CAServer = caConfig,
+//            TemplateName = caTemplate
+//        });
 
-    var issuer = new CertificateIssuer(acdsOptions, new NullLogger<CertificateIssuer>());
-    var (Certificates, Error) = await issuer.IssueCertificate(csrPEM, default);
+//    var issuer = new CertificateIssuer(acdsOptions, new NullLogger<CertificateIssuer>());
+//    var (Certificates, Error) = await issuer.IssueCertificate(csrPEM, default);
 
-    if (Error != null || Certificates == null)
-    {
-        Console.WriteLine(Error?.Detail ?? "Certificate was null, but there was no Error");
-        return;
-    }
+//    if (Error != null || Certificates == null)
+//    {
+//        Console.WriteLine(Error?.Detail ?? "Certificate was null, but there was no Error");
+//        return;
+//    }
 
-    var certificateCollection = new X509Certificate2Collection();
-    certificateCollection.Import(Certificates);
+//    var certificateCollection = new X509Certificate2Collection();
+//    certificateCollection.Import(Certificates);
 
-    var stringBuilder = new StringBuilder();
-    foreach (var certificate in certificateCollection)
-    {
-        var certPem = PemEncoding.Write("CERTIFICATE", certificate.Export(X509ContentType.Cert));
-        stringBuilder.AppendLine(new string(certPem));
-    }
+//    var stringBuilder = new StringBuilder();
+//    foreach (var certificate in certificateCollection)
+//    {
+//        var certPem = PemEncoding.Write("CERTIFICATE", certificate.Export(X509ContentType.Cert));
+//        stringBuilder.AppendLine(new string(certPem));
+//    }
 
-    Console.WriteLine(stringBuilder.ToString());
-}
+//    Console.WriteLine(stringBuilder.ToString());
+//}
