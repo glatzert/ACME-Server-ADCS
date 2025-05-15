f5ef0c2771db5df6084b00060c5186984875c08b
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-05-15 21:20:48 +0200

    Add configuration for device-attest-01 with apple devices

---
+++ src/ACMEServer.Model/Configuration/ChallengeValidationParameters.cs
@@ -0,0 +1,7 @@
+﻿namespace Th11s.ACMEServer.Model.Configuration
+{
+    public class ChallengeValidationParameters
+    {
+        public DeviceAttest01Parameters DeviceAttest01 { get; set; } = new();
+    }
+}
