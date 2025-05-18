9dd9e63dbaef836704ed5b4074727a26b29dbaa2
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-05-18 11:08:01 +0200

    Add EAB requirement to profiles

--- src/ACMEServer.Model/Configuration/ProfileConfiguration.cs
+++ src/ACMEServer.Model/Configuration/ProfileConfiguration.cs
@@ -4,11 +4,17 @@
     {
         public string Name { get; set; } = "";
 
-        public TimeSpan AuthorizationValidityPeriod { get; set; } = TimeSpan.FromDays(1);
 
         public required string[] SupportedIdentifiers { get; set; } = [];
 
+
+        public TimeSpan AuthorizationValidityPeriod { get; set; } = TimeSpan.FromDays(1);
+
+        public bool RequireExternalAccountBinding { get; set; } = false;
+
+
         public required ADCSOptions ADCSOptions { get; set; }
+
 
         public IdentifierValidationParameters IdentifierValidation { get; set; } = new ();
 
