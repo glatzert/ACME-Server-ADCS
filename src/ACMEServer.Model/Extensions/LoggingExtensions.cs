fca8fc9d43e9b4315f5e6dfcdba1a8bee865a18c
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-05-22 22:18:22 +0200

    Add method to make identifiers outputable to strings

---
+++ src/ACMEServer.Model/Extensions/LoggingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Th11s.ACMEServer.Model.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static string AsLogString(this IList<Identifier> identifiers)
+        {
+            if (identifiers == null || identifiers.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder();
+            for(int i = 0; i < identifiers.Count; i++)
+            {
+                var identifier = identifiers[i];
+                sb.Append(identifier.Type);
+                sb.Append(":");
+                sb.Append(identifier.Value);
+
+                if (i < identifiers.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
