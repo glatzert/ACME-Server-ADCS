b6ca7b73629711dad14305a1b0727f93ffa539b6
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-06-05 21:51:26 +0200

    Preserve dotnet license note

--- src/ACMEServer/Services/CertificateSigningRequest/ASN1/Asn1ValueReader.cs
+++ src/ACMEServer/Services/CertificateSigningRequest/ASN1/Asn1ValueReader.cs
@@ -1,4 +1,7 @@
-﻿using System.Formats.Asn1;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Formats.Asn1;
 using System.Numerics;
 
 namespace Th11s.ACMEServer.Services.CertificateSigningRequest.ASN1;
