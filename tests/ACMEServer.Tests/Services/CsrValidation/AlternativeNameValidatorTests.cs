37892417ce7e79cf3a69a7967444657d20e20a74
Ottenhus, Thomas <th11s@outloo.de>
2025-08-19 21:11:34 +0200

    Addind more tests

--- tests/ACMEServer.Tests/Services/CsrValidation/AlternativeNameValidatorTests.cs
+++ tests/ACMEServer.Tests/Services/CsrValidation/AlternativeNameValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging.Abstractions;
 using System.Formats.Asn1;
+using Th11s.ACMEServer.Model;
 using Th11s.ACMEServer.Model.Configuration;
 using Th11s.ACMEServer.Services.Asn1;
 using Th11s.ACMEServer.Services.CsrValidation;
@@ -13,9 +14,8 @@ namespace Th11s.AcmeServer.Tests.Services.CertificateSigningRequest
     /// </summary>
     public class AlternativeNameValidatorTests
     {
-        [Theory,
-            MemberData(nameof(GetAlternativeNames))]
-        public void AnlternativeNameValidator_Will_allow_configured_values(GeneralName generalName, bool expectedResult)
+        [Theory, MemberData(nameof(ConfiguredAlternativeNames))]
+        public void Configured_Parameters_Will_be_used(GeneralName generalName, bool expectedResult)
         {
             var csrValidationParameters = new CSRValidationParameters()
             {
@@ -76,7 +76,7 @@ namespace Th11s.AcmeServer.Tests.Services.CertificateSigningRequest
             Assert.Equal(expectedResult, validationContext.IsAlternativeNameValid(generalName));
         }
 
-        public static IEnumerable<object[]> GetAlternativeNames()
+        public static IEnumerable<object[]> ConfiguredAlternativeNames()
         {
             yield return [GetGeneralName(2, Asn1TestHelpers.CreateDnsName("valid.th11s.it")), true];
             yield return [GetGeneralName(2, Asn1TestHelpers.CreateDnsName("invalid.th11s.it")), false];
@@ -107,6 +107,45 @@ namespace Th11s.AcmeServer.Tests.Services.CertificateSigningRequest
             yield return [GetGeneralName(0, Asn1TestHelpers.CreateOtherName("0.8.15.47.11")), true];
             yield return [GetGeneralName(0, Asn1TestHelpers.CreateOtherName("0.9.16")), false];
         }
+
+
+        [Theory, MemberData(nameof(IdentifierAlternativeNames))]
+        public void Identifier_Alternative_Names_Are_Validated(GeneralName generalName, bool expectedResult)
+        {
+            Identifier[] identifiers = [
+                new (IdentifierTypes.DNS, "valid.th11s.it"),
+                new (IdentifierTypes.IP, "127.0.0.1"),
+                new (IdentifierTypes.IP, "2001:db8:122:344::1"),
+                new (IdentifierTypes.PermanentIdentifier, "valid-permanent-identifier"),
+                // TODO: new (IdentifierTypes.HardwareModule, ""),
+            ];
+
+            var sut = new AlternativeNameValidator(NullLogger.Instance);
+            
+            var validationContext = new CsrValidationContext(identifiers, [generalName], [], []);
+            sut.ValidateWithIdentifiers(validationContext, [generalName], identifiers);
+
+            Assert.Equal(expectedResult, validationContext.IsAlternativeNameValid(generalName));
+        }
+
+        public static IEnumerable<object[]> IdentifierAlternativeNames()
+        {
+            yield return [GetGeneralName(2, Asn1TestHelpers.CreateDnsName("valid.th11s.it")), true];
+            yield return [GetGeneralName(2, Asn1TestHelpers.CreateDnsName("invalid.th11s.it")), false];
+
+            yield return [GetGeneralName(7, Asn1TestHelpers.CreateIpAddress(System.Net.IPAddress.Parse("127.0.0.1"))), true];
+            yield return [GetGeneralName(7, Asn1TestHelpers.CreateIpAddress(System.Net.IPAddress.Parse("2001:db8:122:344::1"))), true];
+            yield return [GetGeneralName(7, Asn1TestHelpers.CreateIpAddress(System.Net.IPAddress.Parse("128.0.0.1"))), false];
+            yield return [GetGeneralName(7, Asn1TestHelpers.CreateIpAddress(System.Net.IPAddress.Parse("2002:db8:122:344::1"))), false];
+
+            yield return [GetGeneralName(0, Asn1TestHelpers.CreatePermanentIdentifier("valid-permanent-identifier", null)), true];
+            yield return [GetGeneralName(0, Asn1TestHelpers.CreatePermanentIdentifier("invalid-permanent-identifier", null)), false];
+
+            // TODO: yield return [GetGeneralName(0, Asn1TestHelpers.CreateHardwareModuleName("0.8.15.47.11", [0, 0, 0, 1])), true];
+            // TODO: yield return [GetGeneralName(0, Asn1TestHelpers.CreateHardwareModuleName("0.9.16", [0, 0, 0, 1])), false];
+        }
+
+
 
         private static GeneralName GetGeneralName(int tagValue, byte[] value)  
             => AlternativeNameFactory.CreateGeneralNameFromAsn1Value(new Asn1Tag(TagClass.ContextSpecific, tagValue), value);
