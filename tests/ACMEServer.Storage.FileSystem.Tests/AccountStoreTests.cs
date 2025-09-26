fbb409b1f242d9505fbb9c536ecf1ba4fe93b594
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-09-26 14:52:27 +0200

    Storage tests

--- tests/ACMEServer.Storage.FileSystem.Tests/AccountStoreTests.cs
+++ tests/ACMEServer.Storage.FileSystem.Tests/AccountStoreTests.cs
@@ -1,5 +1,6 @@
 using ACMEServer.Storage.FileSystem.Configuration;
 using ACMEServer.Tests.Utils;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Th11s.ACMEServer.Model;
 
@@ -14,8 +15,8 @@ public class AccountStoreTests : StoreTestBase
 
         var account = new Account(jwk, ["mailto:some@th11s.de"], DateTimeOffset.Now, null);
 
-        var accountStore = new AccountStore(new OptionsWrapper<FileStoreOptions>(Options));
-        await accountStore.SaveAccountAsync(account, CancellationToken.None);
+        var sut = new AccountStore(new OptionsWrapper<FileStoreOptions>(Options));
+        await sut.SaveAccountAsync(account, CancellationToken.None);
 
         Assert.True(File.Exists(Path.Combine(Options.AccountDirectory, account.AccountId.Value, "account.json")));
         Assert.True(File.Exists(Path.Combine(Options.AccountDirectory, account.Jwk.KeyHash)));
@@ -27,20 +28,66 @@ public class AccountStoreTests : StoreTestBase
         var jwk = JsonWebKeyFactory.CreateRsaJsonWebKey().ToAcmeJwk();
         var account = new Account(jwk, ["mailto:some@th11s.de"], DateTimeOffset.Now, null);
 
-        var accountStore = new AccountStore(new OptionsWrapper<FileStoreOptions>(Options));
-        await accountStore.SaveAccountAsync(account, CancellationToken.None);
+        var sut = new AccountStore(new OptionsWrapper<FileStoreOptions>(Options));
+        await sut.SaveAccountAsync(account, CancellationToken.None);
 
-        var foundAccount = await accountStore.FindAccountAsync(jwk, CancellationToken.None);
+        var foundAccount = await sut.FindAccountAsync(jwk, CancellationToken.None);
         Assert.NotNull(foundAccount);
 
         if(foundAccount != null)
         {
-            Assert.Equal(account.AccountId.Value, foundAccount.AccountId.Value);
+            Assert.Equal(account.AccountId, foundAccount.AccountId);
             Assert.Equal(account.Jwk.KeyHash, foundAccount.Jwk.KeyHash);
             Assert.Equal(account.Contacts, foundAccount.Contacts);
             Assert.Equal(account.TOSAccepted, foundAccount.TOSAccepted);
             Assert.Equal(account.Status, foundAccount.Status);
             Assert.Equal(account.Version, foundAccount.Version);
+        }
+    }
+}
+
+public class OrderStoreTests : StoreTestBase
+{
+    [Fact]
+    public async Task Saving_an_Order_Creates_Order_File_and_Reference_File()
+    {
+        var order = new Order(new(), [new(IdentifierTypes.DNS, "example.th11s.de")]);
+
+        var sut = new OrderStore(new OptionsWrapper<FileStoreOptions>(Options), NullLogger<OrderStore>.Instance);
+        await sut.SaveOrderAsync(order, CancellationToken.None);
+
+        Assert.True(File.Exists(Path.Combine(Options.OrderDirectory, $"{order.OrderId.Value}.json")), "Order was not saved at expected path.");
+        Assert.True(File.Exists(Path.Combine(Options.AccountDirectory, order.AccountId.Value, "orders", order.OrderId.Value)), "Reference File was not saved at expected path.");
+    }
+
+    [Fact]
+    public async Task Saved_Orders_Can_Be_Loaded()
+    {
+        var order = new Order(new(), [new(IdentifierTypes.DNS, "example.th11s.de")]);
+        order.Profile = new ("some-profile");
+
+        var sut = new OrderStore(new OptionsWrapper<FileStoreOptions>(Options), NullLogger<OrderStore>.Instance);
+        await sut.SaveOrderAsync(order, CancellationToken.None);
+
+        var loadedOrder = await sut.LoadOrderAsync(order.OrderId, CancellationToken.None);
+        Assert.NotNull(loadedOrder);
+
+        if(loadedOrder != null)
+        {
+            Assert.Equal(order.OrderId, loadedOrder.OrderId);
+            Assert.Equal(order.AccountId, loadedOrder.AccountId);
+            Assert.Equal(order.Status, loadedOrder.Status);
+            Assert.Equal(order.Expires, loadedOrder.Expires);
+
+            Assert.Equal(order.Identifiers.Count, loadedOrder.Identifiers.Count);
+            Assert.Equal(order.Authorizations.Count, loadedOrder.Authorizations.Count);
+
+            Assert.Equal(order.NotAfter, loadedOrder.NotAfter);
+            Assert.Equal(order.NotBefore, loadedOrder.NotBefore);
+            Assert.Equal(order.Profile, loadedOrder.Profile);
+            Assert.Equal(order.Error, loadedOrder.Error);
+
+            Assert.Equal(order.Version, loadedOrder.Version);
         }
     }
 }
