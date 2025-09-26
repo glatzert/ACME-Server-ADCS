64c7c5a1415f14ec356001cd20e13a61142bb171
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-09-26 14:19:06 +0200

    Rename and fix a small flaw

--- src/ACMEServer.Model/Primitives/ResourceIdentifier.cs
+++ src/ACMEServer.Model/Primitives/ResourceIdentifier.cs
@@ -1,7 +1,9 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Th11s.ACMEServer.Model.Primitives;
 
+[DebuggerStepThrough]
 public abstract record class ResourceIdentifier
 {
     private static Regex IdentifierRegex { get; } = new("^[A-Za-z0-9_-]{5,64}$", RegexOptions.Compiled);
