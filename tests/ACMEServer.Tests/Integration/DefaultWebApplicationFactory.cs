1ac68170e8e137b40e2dd1de805a9f397627cd88
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-10-02 11:04:38 +0200

    Missing using

--- tests/ACMEServer.Tests/Integration/DefaultWebApplicationFactory.cs
+++ tests/ACMEServer.Tests/Integration/DefaultWebApplicationFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Th11s.AcmeServer.Tests.Integration.Fakes;
 using Th11s.ACMEServer.Configuration;
 using Th11s.ACMEServer.Model;
 using Th11s.ACMEServer.Services;
