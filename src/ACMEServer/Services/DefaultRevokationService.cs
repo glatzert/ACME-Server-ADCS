d19c4402bea50516d4f227dd7469da19d85ba8ff
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-10-06 21:06:41 +0200

    REvokation service stub

---
+++ src/ACMEServer/Services/DefaultRevokationService.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Th11s.ACMEServer.HttpModel.Payloads;
+using Th11s.ACMEServer.Model;
+using Th11s.ACMEServer.Model.JWS;
+using Th11s.ACMEServer.Model.Primitives;
+using Th11s.ACMEServer.Model.Storage;
+
+namespace Th11s.ACMEServer.Services
+{
+    public class DefaultRevokationService(ICertificateStore certificateStore) : IRevokationService
+    {
+        private readonly ICertificateStore _certificateStore = certificateStore;
+
+        public async Task RevokeCertificateAsync(AcmeJwsToken acmeRequest, RevokeCertificate payload, CancellationToken cancellationToken)
+        {
+            var certificateBytes = Convert.FromBase64String(payload.Certificate);
+            var certificate = new X509Certificate2(certificateBytes);
+
+            var certificateId = CertificateId.FromX509Certificate(certificate);
+
+            // First we locate the certificate to be revoked.
+            var orderCertificates = await _certificateStore.LoadCertificateAsync(certificateId, cancellationToken) 
+                ?? throw AcmeErrors.MalformedRequest("The specified certificate was not found.").AsException();
+
+
+            // We now need to check if it's signed by the account that owns the certificate or the certificate itself.
+            var isAuthorized = await IsRevokationAuthorizedAsync(acmeRequest, payload, cancellationToken);
+
+            if (!isAuthorized)
+            {
+                throw AcmeErrors.Unauthorized().AsException();
+            }
+        }
+
+        private Task<bool> IsRevokationAuthorizedAsync(AcmeJwsToken acmeRequest, RevokeCertificate payload, CancellationToken cancellationToken)
+        {
+            if (acmeRequest.AcmeHeader.Kid is not null)
+            {
+                // The request is signed by an account.
+                var accountId = acmeRequest.AcmeHeader.GetAccountId();
+
+            }
+        }
+    }
+}
