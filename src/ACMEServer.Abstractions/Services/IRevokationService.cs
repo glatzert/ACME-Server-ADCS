b04de9603f7facc6b7909e54170d81d453242487
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-10-06 21:05:51 +0200

    Revokation interface and payload

---
+++ src/ACMEServer.Abstractions/Services/IRevokationService.cs
@@ -0,0 +1,9 @@
+﻿using Th11s.ACMEServer.Model.JWS;
+using Payloads = Th11s.ACMEServer.HttpModel.Payloads;
+
+namespace Th11s.ACMEServer.Services;
+
+public interface IRevokationService
+{
+    Task RevokeCertificateAsync(AcmeJwsToken acmeRequest, Payloads.RevokeCertificate payload, CancellationToken cancellationToken);
+}
