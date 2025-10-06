ed71153eac20cd13fb05234c47394e9c50eb6d30
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-10-06 21:06:18 +0200

    Allow claim access via extension method

--- src/ACMEServer/AspNetCore/Extensions/ClaimsPrincipalExtensions.cs
+++ src/ACMEServer/AspNetCore/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,9 +6,12 @@ namespace Th11s.ACMEServer.AspNetCore.Extensions;
 
 public static class ClaimsPrincipalExtensions
 {
+    public static Claim? GetAccountIdClaim(this ClaimsPrincipal principal)
+        => principal.Claims.FirstOrDefault(c => c.Type == AcmeClaimTypes.AccountId);
+
     public static AccountId GetAccountId(this ClaimsPrincipal principal)
     {
-        var accountIdClaim = principal.Claims.FirstOrDefault(c => c.Type == AcmeClaimTypes.AccountId);
+        var accountIdClaim = principal.GetAccountIdClaim();
         if (accountIdClaim == null)
         {
             throw new InvalidOperationException("No account ID found in claims.");
