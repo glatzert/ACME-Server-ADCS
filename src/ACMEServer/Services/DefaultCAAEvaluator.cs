312840fc5894a79b6fd6fd59f434ebb151f49663
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-10-22 22:21:21 +0200

    Class stub

---
+++ src/ACMEServer/Services/DefaultCAAEvaluator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Th11s.ACMEServer.Configuration;
+using Th11s.ACMEServer.Model;
+
+namespace Th11s.ACMEServer.Services;
+
+public class DefaultCAAEvaluator(
+    ICAAQueryHandler caaQueryHandler, 
+    IOptions<ACMEServerOptions> options,
+    ILogger<DefaultCAAEvaluator> logger
+    ) : ICAAEvaluator
+{
+    private readonly ICAAQueryHandler _caaQueryHandler = caaQueryHandler;
+    private readonly IOptions<ACMEServerOptions> _options = options;
+    private readonly ILogger<DefaultCAAEvaluator> _logger = logger;
+
+    public Task<bool> HasValidCAARecord(Identifier identifier)
+    {
+        throw new NotImplementedException();
+    }
+}
