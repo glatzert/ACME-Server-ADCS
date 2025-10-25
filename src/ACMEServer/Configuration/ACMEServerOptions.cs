8627c04e7c6379193cac2aa278deba9c87d9690d
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-10-25 12:52:09 +0200

    CHORE: collection expressions

--- src/ACMEServer/Configuration/ACMEServerOptions.cs
+++ src/ACMEServer/Configuration/ACMEServerOptions.cs
@@ -7,7 +7,7 @@ public class ACMEServerOptions
     // TODO: Background checking the status of orders and authorizations is probably not neccessary anymore - we should only do it upon startup to populate the queues.
     public BackgroundServiceOptions HostedWorkers { get; set; } = new ();
 
-    public string[] CAAIdentities { get; set; } = Array.Empty<string>();
+    public string[] CAAIdentities { get; set; } = [];
 
     public string? WebsiteUrl { get; set; }
 
