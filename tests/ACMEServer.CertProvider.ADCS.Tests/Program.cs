b7fe7815b26096c7b17c8cb8cc5244784488f96d
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-11-04 21:13:07 +0100

    Add issuance module test application

---
+++ tests/ACMEServer.CertProvider.ADCS.Tests/Program.cs
@@ -0,0 +1,9 @@
+﻿using Windows.Win32;
+using Windows.Win32.Security.Cryptography.Certificates;
+
+//PInvoke.CoCreateInstance<ICertAdmin>()
+
+var certRequest = CCertRequest.CreateInstance<ICertRequest3>();
+var certAdmin = CCertAdmin.CreateInstance<ICertAdmin>();
+
+
