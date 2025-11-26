b7e304bb432deb4f600446f16bc7b1bbd06f3915
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-11-26 22:21:23 +0100

    Version prompt

--- build.ps1
+++ build.ps1
@@ -1,9 +1,17 @@
+param(
+	[string]$Version = ""
+)
+
+if($Version -eq "") {
+	$Version = Read-Host "Enter version number (e.g. 1.2.3)"
+}
+
 dotnet restore ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj
 
 @("8","10") | % {
-	dotnet build ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj --no-restore --no-incremental -c Release --framework net$_.0-windows
-	dotnet publish ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj --no-build --framework net$_.0-windows -c Release -o ./artifacts/net$_.0
+	dotnet build ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj -c Release --framework net$_.0-windows --no-restore --no-incremental 
+	dotnet publish ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj  --framework net$_.0-windows -c Release -o ./artifacts/net$_.0 --no-build
 
 	Compress-Archive -Path ./artifacts/net$_.0/* -DestinationPath ./artifacts/ACMEServer.ADCS-Vx-net$_.0.zip -Force
-	Write-Host "Created ./artifacts/ACMEServer.ADCS-Vx-net$_.0.zip"
+	Write-Host "Created ./artifacts/ACMEServer.ADCS-V$Version-net$_.0.zip"
 }
