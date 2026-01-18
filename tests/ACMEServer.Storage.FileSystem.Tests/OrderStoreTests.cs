d244be4d9589aef22ef25ab8617ba09adc38ac05
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-01-18 21:16:51 +0100

    Adjust a test

--- tests/ACMEServer.Storage.FileSystem.Tests/OrderStoreTests.cs
+++ tests/ACMEServer.Storage.FileSystem.Tests/OrderStoreTests.cs
@@ -119,10 +119,10 @@ public class OrderStoreTests : StoreTestBase
 
         Assert.NotNull(loadedOrder);
 
-        Assert.Equal(order.OrderId, loadedOrder.OrderId);
-        Assert.Equal(order.AccountId, loadedOrder.AccountId);
+            Assert.Equal(order.OrderId, loadedOrder.OrderId);
+            Assert.Equal(order.AccountId, loadedOrder.AccountId);
         
-        Assert.Equal(order.Status, loadedOrder.Status);
+            Assert.Equal(order.Status, loadedOrder.Status);
 
         for(int i = 0; i < order.Identifiers.Count; i++)
         {
@@ -156,21 +156,21 @@ public class OrderStoreTests : StoreTestBase
                 Assert.Equal(expectedChallenge.Token, actualChallenge.Token);
                 Assert.Equal(expectedChallenge.Payload, actualChallenge.Payload);
                 Assert.Equal(expectedChallenge.Validated, actualChallenge.Validated);
-            
+
                 Assert.Equivalent(expectedChallenge.Error, actualChallenge.Error, strict: true);
             }
         }
 
         Assert.Equal(order.NotBefore, loadedOrder.NotBefore);
-        Assert.Equal(order.NotAfter, loadedOrder.NotAfter);
+            Assert.Equal(order.NotAfter, loadedOrder.NotAfter);
         Assert.Equal(order.Expires, loadedOrder.Expires);
 
-        Assert.Equal(order.Profile, loadedOrder.Profile);
+            Assert.Equal(order.Profile, loadedOrder.Profile);
         Assert.Equal(order.CertificateSigningRequest, loadedOrder.CertificateSigningRequest);
 
         Assert.Equivalent(order.Error, loadedOrder.Error, strict: true);
-        Assert.Equal(order.Version, loadedOrder.Version);
-    }
+            Assert.Equal(order.Version, loadedOrder.Version);
+        }
 
     [Theory,
         InlineData(OrderJsonFileVariants.Order_SV1_FullModel)]
@@ -206,107 +206,137 @@ public class OrderStoreTests : StoreTestBase
             Assert.Equal(638998550596190265, loadedOrder.Version);
         }
     }
-}
+    }
 
 internal static class OrderJsonFileVariants
-{
-    public const string Order_SV1_FullModel = """
     {
-        "$id": "1",
-        "SerializationVersion": 2,
-        "OrderId": "pqyaNlRFVUakjv8Rr36I3Q",
-        "AccountId": "Y47V6u5uoEu-GNAuWaQ16w",
-        "Status": "Valid",
-        "Identifiers": {
-            "$id": "2",
-            "$values": [
-                {
-                    "$id": "3",
-                    "SerializationVersion": 1,
-                    "Type": "dns",
-                    "Value": "site.web.th11s.corp",
-                    "Metadata": {
-                        "$id": "4"
-                    }
-                },
-                {
-                    "$id": "5",
-                    "SerializationVersion": 1,
-                    "Type": "dns",
-                    "Value": "site-a.web.th11s.corp",
-                    "Metadata": {
-                        "$id": "6"
-                    }
-                }
-            ]
-        },
-        "Authorizations": {
-            "$id": "7",
-            "$values": [
-                {
-                    "$id": "8",
-                    "SerializationVersion": 1,
-                    "AuthorizationId": "hyvU3onKkEy1SkI12oexPg",
-                    "Status": "Valid",
-                    "Identifier": {
-                        "$ref": "3"
+    public const string Order_SV1_FullModel = """
+        {
+            "$id": "1",
+            "SerializationVersion": 2,
+            "OrderId": "pqyaNlRFVUakjv8Rr36I3Q",
+            "AccountId": "Y47V6u5uoEu-GNAuWaQ16w",
+            "Status": "Valid",
+            "Identifiers": {
+                "$id": "2",
+                "$values": [
+                    {
+                        "$id": "3",
+                        "SerializationVersion": 1,
+                        "Type": "dns",
+                        "Value": "site.web.th11s.corp",
+                        "Metadata": {
+                            "$id": "4"
+                        }
                     },
-                    "IsWildcard": false,
-                    "Expires": "2025-11-28T16:43:54.5204447+01:00",
-                    "Challenges": {
-                        "$id": "9",
-                        "$values": [
-                            {
-                                "$id": "10",
-                                "SerializationVersion": 1,
-                                "ChallengeId": "PeSCw0UVGEWDP3xwAR2hGQ",
-                                "Status": "Valid",
-                                "Type": "http-01",
-                                "Token": "-AdmkBybHNzwpGwqJUa56biHhkjtUXEF8XmZJVxsmQ5g9Ho3lfa_rqB1BR2zG35v",
-                                "Payload": "e30",
-                                "Validated": "2025-11-27T16:43:54.7569754+01:00",
-                                "Error": null
-                            }
-                        ]
+                    {
+                        "$id": "5",
+                        "SerializationVersion": 1,
+                        "Type": "dns",
+                        "Value": "site-a.web.th11s.corp",
+                        "Metadata": {
+                            "$id": "6"
+                        }
                     }
-                },
-                {
-                    "$id": "11",
-                    "SerializationVersion": 1,
-                    "AuthorizationId": "okoUHL0ae0Cxc53SxV9WOA",
-                    "Status": "Valid",
-                    "Identifier": {
-                        "$ref": "5"
+                ]
+            },
+            "Authorizations": {
+                "$id": "7",
+                "$values": [
+                    {
+                        "$id": "8",
+                        "SerializationVersion": 1,
+                        "AuthorizationId": "hyvU3onKkEy1SkI12oexPg",
+                        "Status": "Valid",
+                        "Identifier": {
+                            "$ref": "3"
+                        },
+                        "IsWildcard": false,
+                        "Expires": "2025-11-28T16:43:54.5204447+01:00",
+                        "Challenges": {
+                            "$id": "9",
+                            "$values": [
+                                {
+                                    "$id": "10",
+                                    "SerializationVersion": 1,
+                                    "ChallengeId": "PeSCw0UVGEWDP3xwAR2hGQ",
+                                    "Status": "Valid",
+                                    "Type": "http-01",
+                                    "Token": "-AdmkBybHNzwpGwqJUa56biHhkjtUXEF8XmZJVxsmQ5g9Ho3lfa_rqB1BR2zG35v",
+                                    "Payload": "e30",
+                                    "Validated": "2025-11-27T16:43:54.7569754+01:00",
+                                    "Error": null
+                                }
+                            ]
+                        }
                     },
-                    "IsWildcard": false,
-                    "Expires": "2025-11-28T16:43:54.5204447+01:00",
-                    "Challenges": {
-                        "$id": "12",
-                        "$values": [
-                            {
-                                "$id": "13",
-                                "SerializationVersion": 1,
-                                "ChallengeId": "cXscQ-fPwEe8keZ6cx9byg",
-                                "Status": "Valid",
-                                "Type": "http-01",
-                                "Token": "mFXNL02t38jWvHD99F2lZd2rA_Oo0kXLQX49HixPV6kveAzbQPmzZwvyQOAfm7GN",
-                                "Payload": "e30",
-                                "Validated": "2025-11-27T16:43:59.7110854+01:00",
-                                "Error": null
-                            }
-                        ]
+                    {
+                        "$id": "11",
+                        "SerializationVersion": 1,
+                        "AuthorizationId": "okoUHL0ae0Cxc53SxV9WOA",
+                        "Status": "Valid",
+                        "Identifier": {
+                            "$ref": "5"
+                        },
+                        "IsWildcard": false,
+                        "Expires": "2025-11-28T16:43:54.5204447+01:00",
+                        "Challenges": {
+                            "$id": "12",
+                            "$values": [
+                                {
+                                    "$id": "13",
+                                    "SerializationVersion": 1,
+                                    "ChallengeId": "cXscQ-fPwEe8keZ6cx9byg",
+                                    "Status": "Valid",
+                                    "Type": "http-01",
+                                    "Token": "mFXNL02t38jWvHD99F2lZd2rA_Oo0kXLQX49HixPV6kveAzbQPmzZwvyQOAfm7GN",
+                                    "Payload": "e30",
+                                    "Validated": "2025-11-27T16:43:59.7110854+01:00",
+                                    "Error": null
+                                }
+                            ]
+                        }
                     }
-                }
-            ]
-        },
-        "NotBefore": null,
-        "NotAfter": null,
-        "Expires": "2027-11-27T16:34:11+01:00",
-        "Profile": "Default-DNS",
-        "Error": null,
-        "Version": 638998550596190265,
-        "CertificateSigningRequest": "MIIDqzCCAhMCAQAwHjEcMBoGA1UEAwwTc2l0ZS53ZWIudGgxMXMuY29ycDCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAIhl5YrTTonUAGaqx82pbFMpnggvSbGX444t16EnlToT3IV6P-RjQSgOYjnkC8xcTmcJs-ZTSYcDiI310im485ZzfUBmkBT2wXqXHn9g7n3E0GyCVx4LVf8lSbJZIEyzKvLINNpjKu4bAel6vAiDHnOkHM_ySnCLVnzt9Tc4hTxQvrLvs7bUO1M7Tpt5hrV29dCQRreuvPfa10CjVoWOYiz2ufb-KVDvoK4qIqq7M0zWdjs-hRnKdKX2MmI8L3NiNWVTUlTn4YuCE9003Fma1N-bRfpuUbi3-goXOtQx2QN9sVDCIfVQQzN7cL-N9MkzuMNvgEy3w-XoNFtz_-XzH0St8rqSu0SSnLaycYcgPPnj97QUkTR-Du5Oj-EzBU4KMY2XJgwvikzDlWtAQGl5z7sLIpSCgvCd4VLs9TmTbQ4yHMUn0lHd3Ek_JDyFViwsyNISmDSFFoH1w0ql7_jpK8RU0V8zleR-0aEOjpZ6pVt9r20ATtEygAx47fV2UJXGmQIDAQABoEgwRgYJKoZIhvcNAQkOMTkwNzA1BgNVHREELjAsghNzaXRlLndlYi50aDExcy5jb3JwghVzaXRlLWEud2ViLnRoMTFzLmNvcnAwDQYJKoZIhvcNAQENBQADggGBAEMR_LKFhWYHpzW4hjpv35ehSLZL-ii2M_1p5s5oiig59g0KtjiXmr3WSPRlMS072vxMjLDx3VWBmND7dmu7CWnxPPLM9Toi0kY4kw6uknJHQfzeF2e_mC0NGjJPO0zc1fX3Bphn3qERsr_GiOVI7poSNOpQuBeVJwR-Tbk9wxPW21Kxct-3jQn25ok11olWFKUqO3kjaSqm0AJOpXKKq5woFeDRMRZicNn9mje4Ci4PLDX-EdIaWJ2o5LdaacNEBj1ylVAMMiLIx3aYBkRLjuSUvNqSkibJBXUDANiWq4uprIr5L9VA2g09twXi4_kxhqtGZHzzJrC8Q4wokuLIKsR6DFOEAFGAtaeT_jf72Pqu8kA9riY82ZGbTeVJK2Z2ftgNesnf2VkwJuFKUpFmVdSedxDIlEeIXXIJojlAfUgxdFv18SXcdXjdsXMJ9V_nzcI1lyPnUgelpssvpNwor6MvBDJxkHdseoNxs_YnY5xrIU4p91URa44nB-m4gz2Q-Q",
-        "CertificateId": "AA2uUdvkKt8WN56E0rTWXs1Yybo.OAAAAEQ9nH1TRsvuHQAAAAAARA"
+                ]
+            },
+            "NotBefore": null,
+            "NotAfter": null,
+            "Expires": "2027-11-27T16:34:11+01:00",
+            "Profile": "Default-DNS",
+            "Error": null,
+            "Version": 638998550596190265,
+            "CertificateSigningRequest": "MIIDqzCCAhMCAQAwHjEcMBoGA1UEAwwTc2l0ZS53ZWIudGgxMXMuY29ycDCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAIhl5YrTTonUAGaqx82pbFMpnggvSbGX444t16EnlToT3IV6P-RjQSgOYjnkC8xcTmcJs-ZTSYcDiI310im485ZzfUBmkBT2wXqXHn9g7n3E0GyCVx4LVf8lSbJZIEyzKvLINNpjKu4bAel6vAiDHnOkHM_ySnCLVnzt9Tc4hTxQvrLvs7bUO1M7Tpt5hrV29dCQRreuvPfa10CjVoWOYiz2ufb-KVDvoK4qIqq7M0zWdjs-hRnKdKX2MmI8L3NiNWVTUlTn4YuCE9003Fma1N-bRfpuUbi3-goXOtQx2QN9sVDCIfVQQzN7cL-N9MkzuMNvgEy3w-XoNFtz_-XzH0St8rqSu0SSnLaycYcgPPnj97QUkTR-Du5Oj-EzBU4KMY2XJgwvikzDlWtAQGl5z7sLIpSCgvCd4VLs9TmTbQ4yHMUn0lHd3Ek_JDyFViwsyNISmDSFFoH1w0ql7_jpK8RU0V8zleR-0aEOjpZ6pVt9r20ATtEygAx47fV2UJXGmQIDAQABoEgwRgYJKoZIhvcNAQkOMTkwNzA1BgNVHREELjAsghNzaXRlLndlYi50aDExcy5jb3JwghVzaXRlLWEud2ViLnRoMTFzLmNvcnAwDQYJKoZIhvcNAQENBQADggGBAEMR_LKFhWYHpzW4hjpv35ehSLZL-ii2M_1p5s5oiig59g0KtjiXmr3WSPRlMS072vxMjLDx3VWBmND7dmu7CWnxPPLM9Toi0kY4kw6uknJHQfzeF2e_mC0NGjJPO0zc1fX3Bphn3qERsr_GiOVI7poSNOpQuBeVJwR-Tbk9wxPW21Kxct-3jQn25ok11olWFKUqO3kjaSqm0AJOpXKKq5woFeDRMRZicNn9mje4Ci4PLDX-EdIaWJ2o5LdaacNEBj1ylVAMMiLIx3aYBkRLjuSUvNqSkibJBXUDANiWq4uprIr5L9VA2g09twXi4_kxhqtGZHzzJrC8Q4wokuLIKsR6DFOEAFGAtaeT_jf72Pqu8kA9riY82ZGbTeVJK2Z2ftgNesnf2VkwJuFKUpFmVdSedxDIlEeIXXIJojlAfUgxdFv18SXcdXjdsXMJ9V_nzcI1lyPnUgelpssvpNwor6MvBDJxkHdseoNxs_YnY5xrIU4p91URa44nB-m4gz2Q-Q",
+            "CertificateId": "AA2uUdvkKt8WN56E0rTWXs1Yybo.OAAAAEQ9nH1TRsvuHQAAAAAARA"
+        }
+        """;
+        Directory.CreateDirectory(Options.OrderDirectory);
+
+        var orderId = new OrderId("pqyaNlRFVUakjv8Rr36I3Q");
+        var orderFilePath = Path.Combine(Options.OrderDirectory, $"{orderId.Value}.json");
+        await File.WriteAllTextAsync(orderFilePath, orderContent, TestContext.Current.CancellationToken);
+
+        var sut = new OrderStore(new OptionsWrapper<FileStoreOptions>(Options), NullLogger<OrderStore>.Instance);
+        var loadedOrder = await sut.LoadOrderAsync(orderId, TestContext.Current.CancellationToken);
+
+        Assert.NotNull(loadedOrder);
+
+        if (loadedOrder != null)
+        {
+            Assert.Equal(orderId, loadedOrder.OrderId);
+            Assert.Equal("Y47V6u5uoEu-GNAuWaQ16w", loadedOrder.AccountId.Value);
+            Assert.Equal(OrderStatus.Valid, loadedOrder.Status);
+            Assert.Equal(DateTimeOffset.Parse("2027-11-27T16:34:11+01:00"), loadedOrder.Expires);
+
+            Assert.Equal(2, loadedOrder.Identifiers.Count);
+            Assert.Equal(2, loadedOrder.Authorizations.Count);
+            Assert.Equal(loadedOrder.Authorizations.FirstOrDefault()?.Identifier, loadedOrder.Identifiers.FirstOrDefault());
+
+            Assert.Null(loadedOrder.NotAfter);
+            Assert.Null(loadedOrder.NotBefore);
+            Assert.Equal("Default-DNS", loadedOrder.Profile.Value);
+            Assert.Null(loadedOrder.Error);
+
+            Assert.Equal(638998550596190265, loadedOrder.Version);
+        }
     }
-    """;
 }
