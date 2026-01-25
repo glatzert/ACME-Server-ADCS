f28f7f3d487e3e89f1ad3ea380bb2275aa437312
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-01-25 09:20:23 +0100

    Use version number from prompt

--- build.ps1
+++ build.ps1
@@ -12,6 +12,6 @@ dotnet restore ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj
 	dotnet build ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj -c Release --framework net$_.0-windows --no-restore --no-incremental 
 	dotnet publish ./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj  --framework net$_.0-windows -c Release -o ./artifacts/net$_.0 --no-build
 
-	Compress-Archive -Path ./artifacts/net$_.0/* -DestinationPath ./artifacts/ACMEServer.ADCS-Vx-net$_.0.zip -Force
-	Write-Host "Created ./artifacts/ACMEServer.ADCS-V$Version-net$_.0.zip"
+	Compress-Archive -Path ./artifacts/net$_.0/* -DestinationPath ./artifacts/ACMEServer.ADCS-V$($Version)-net$_.0.zip -Force
+	Write-Host "Created ./artifacts/ACMEServer.ADCS-V$($Version)-net$_.0.zip"
 }
