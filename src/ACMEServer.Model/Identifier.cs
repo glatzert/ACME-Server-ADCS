3de83267cdf888cf3c989ff15b4b7a790e924b08
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-01-27 12:47:08 +0100

    Replace identifier class with record

--- src/ACMEServer.Model/Identifier.cs
+++ src/ACMEServer.Model/Identifier.cs
@@ -1,46 +1,9 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Th11s.ACMEServer.Model.Exceptions;
+﻿namespace Th11s.ACMEServer.Model;
 
-namespace Th11s.ACMEServer.Model;
-
-public class Identifier
+public record Identifier(string Type, string Value)
 {
-    private string? _type;
-    private string? _value;
-
-    [SetsRequiredMembers]
-    public Identifier(string type, string value)
-    {
-        Type = type;
-        Value = value;
-    }
-
-    public required string Type
-    {
-        get => _type ?? throw new NotInitializedException();
-        init
-        {
-            // TODO: This should probably happen in the minimal API endpoint.
-            var normalizedType = value?.Trim();
-            _type = normalizedType;
-        }
-    }
-
-    public required string Value
-    {
-        get => _value ?? throw new NotInitializedException();
-        set => _value = !string.IsNullOrWhiteSpace(value) ? value : null;
-    }
-
     public override string ToString()
     {
         return $"{Type}:{Value}";
-    }
-
-    
-    public static class MetadataKeys
-    {
-        public const string PublicKey = "expected-public-key";
-        public const string CAAValidationMehods = "caa-validation-methods";
     }
 }
