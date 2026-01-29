729bf36364d007898a0d3f1202af2692ef3ce9a2
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-01-29 12:19:02 +0100

    Create a new challenge type (persist-dns-01)

--- src/ACMEServer.Model/ChallengeTypes.cs
+++ src/ACMEServer.Model/ChallengeTypes.cs
@@ -6,7 +6,8 @@ public static class ChallengeTypes
     public const string Dns01 = "dns-01";
     public const string TlsAlpn01 = "tls-alpn-01";
     public const string DeviceAttest01 = "device-attest-01";
+    public const string DnsPersist01 = "dns-persist-01";
 
-    public static readonly string[] AllTypes = [Http01, Dns01, TlsAlpn01, DeviceAttest01];
+    public static readonly string[] AllTypes = [Http01, Dns01, TlsAlpn01, DeviceAttest01, DnsPersist01];
     public static readonly string[] TokenChallenges = [Http01, Dns01, TlsAlpn01, DeviceAttest01];
 }
