6efbb7d6282f3e5206b8ff4fcc0adedaace555a4
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-01-31 12:16:37 +0100

    Provide a test fake

---
+++ tests/ACMEServer.Tests.Utils/Fakes/FakeProfileProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Th11s.ACMEServer.Model.Configuration;
+using Th11s.ACMEServer.Model.Primitives;
+using Th11s.ACMEServer.Services;
+
+namespace Th11s.ACMEServer.Tests.Utils.Fakes
+{
+    public class FakeProfileProvider : IProfileProvider
+    {
+        private readonly Dictionary<ProfileName, ProfileConfiguration> _configurations;
+
+        public FakeProfileProvider(Dictionary<ProfileName, ProfileConfiguration> configurations)
+        {
+            _configurations = configurations;
+        }
+
+        public ProfileConfiguration GetProfileConfiguration(ProfileName profileName)
+            => _configurations[profileName];
+
+        public IEnumerable<ProfileName> GetProfileNames()
+            => _configurations.Keys;
+
+        public bool TryGetProfileConfiguration(ProfileName profileName, [NotNullWhen(true)] out ProfileConfiguration? profileConfiguration)
+            => _configurations.TryGetValue(profileName, out profileConfiguration);
+    }
+}
