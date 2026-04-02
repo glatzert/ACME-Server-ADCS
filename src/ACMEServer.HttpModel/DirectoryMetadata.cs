8025e7208c604fe9b0cdb75df0b9a75778087c03
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-04-02 22:35:51 +0200

    Nullability patch

--- src/ACMEServer.HttpModel/DirectoryMetadata.cs
+++ src/ACMEServer.HttpModel/DirectoryMetadata.cs
@@ -11,7 +11,7 @@ public class DirectoryMetadata
     public string[]? CAAIdentities { get; set; }
     public required bool ExternalAccountRequired { get; set; }
 
-    public required Dictionary<string, string?> Profiles { get; set; }
+    public required Dictionary<string, string> Profiles { get; set; }
 }
 
 
