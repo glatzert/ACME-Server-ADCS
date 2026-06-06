e018c2fe19bac40c7f7d2735af84fb632b9149ae
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-06-06 23:27:57 +0200

    Making a note

--- src/ACMEServer/Services/DefaultPublicKeyAnalyzer.cs
+++ src/ACMEServer/Services/DefaultPublicKeyAnalyzer.cs
@@ -26,6 +26,7 @@ public class DefaultPublicKeyAnalyzer(ILogger<DefaultPublicKeyAnalyzer> logger) 
             return null;
         }
 
+        // TODO: switch on certificateRequest.PublicKey.Oid
         if (certificateRequest.PublicKey.GetRSAPublicKey() is RSA rsaPublicKey)
         {
             return new PublicKeyInfo("RSA", rsaPublicKey.KeySize);
