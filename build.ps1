19f874046fca63435ae50fb7260afa97277e5857
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-07-21 20:46:30 +0200

    Refine build process and regenerate BOM

--- build.ps1
+++ build.ps1
@@ -3,23 +3,26 @@ param(
 )
 
 $projectFile = Resolve-Path "./src/ACMEServer.ADCS/ACMEServer.ADCS.csproj"
+$projectFileXml = [xml](Get-Content $projectFile)
 if ($Version -eq "") {
-	$Version = ([xml](Get-Content $projectFile)).Project.PropertyGroup.Version
+	$Version = $projectFileXml.Project.PropertyGroup.Version
 
 	if ($Version -eq "") {
 		$Version = Read-Host "Enter version number (e.g. 1.2.3)"
 	}
 }
 
-dotnet restore $projectFile
+$targetFrameworks = $projectFileXml.Project.PropertyGroup.TargetFrameworks -split ';'
 
-@("8","10") | % {
-	$outPath = [IO.Path]::Combine((Resolve-Path ./artifacts/), "net$_.0")
+dotnet restore $projectFile /p:GenerateSBOM=true
 
-	dotnet build $projectFile -c Release --framework net$_.0-windows --no-restore --no-incremental 
-	dotnet publish $projectFile  --framework net$_.0-windows -c Release -o $outPath --no-build
+foreach ($targetFramework in $targetFrameworks) {
+	$outPath = [IO.Path]::Combine((Resolve-Path ./artifacts/), $targetFramework)
 
-	$archivePath = [IO.Path]::Combine((Resolve-Path ./artifacts/), "ACMEServer.ADCS-V$Version-net$_.0.zip")
+	dotnet build $projectFile -c Release --framework $targetFramework --no-restore --no-incremental 
+	dotnet publish $projectFile  --framework $targetFramework -c Release -o $outPath --no-build
+
+	$archivePath = [IO.Path]::Combine((Resolve-Path ./artifacts/), "ACMEServer.ADCS-V$Version-$targetFramework.zip")
 	Compress-Archive -Path $outPath/* -DestinationPath $archivePath -Force
 
 	Write-Host "Created $archivePath"
