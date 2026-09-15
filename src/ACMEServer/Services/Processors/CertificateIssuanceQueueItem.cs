515335b1836391fe7a3fb899545671908c5afaa7
Ottenhus, Thomas <TG85+Github@outlook.de>
2026-09-15 23:19:55 +0200

    Allow a TCS source to yield back from the working queue to the caller

---
+++ src/ACMEServer/Services/Processors/CertificateIssuanceQueueItem.cs
@@ -0,0 +1,11 @@
+﻿using Th11s.ACMEServer.Model;
+using Th11s.ACMEServer.Model.Primitives;
+
+namespace Th11s.ACMEServer.Services.Processors
+{
+    public class CertificateIssuanceQueueItem(OrderId orderId, TaskCompletionSource<Order>? issuanceCompletionSource)
+    {
+        public OrderId OrderId { get; } = orderId;
+        public TaskCompletionSource<Order>? IssuanceCompletionSource { get; } = issuanceCompletionSource;
+    }
+}
